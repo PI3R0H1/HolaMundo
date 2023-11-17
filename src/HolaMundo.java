625aa8306c57c2ff043c9c686c112953b8c957c4
PI3R0H1 <pierohi733@gmail.com>
2023-11-16 19:48:09 -0500

    Primer Commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,8 @@
+//MI clase en java
+public class HolaMundo {
+    public static void main(String args[]){
+        int miVaribleEntera = 10;
+        System.out.println (miVaribleEntera);
+    }
+}
+ 
